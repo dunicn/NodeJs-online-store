12d43e7a4c7e00603106b4baed1e858975bfb63a
dunicn <nemanjazdunic@gmail.com>
2021-07-12 09:32:01 +0200

    added flash message - user feedback when signing in and signing up

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const mongoDbStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const controllers404 = require('./controllers/404');
 const User = require('./models/user');
@@ -37,6 +38,7 @@ app.use(
   })
 );
 app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
